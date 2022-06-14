805bc862e7deec6c8303cffd4143715ab41f09cd
EvoX7 <creativesoultouch@gmail.com>
2022-06-14 18:53:51 +0200

    Final commit

--- js/main.js
+++ js/main.js
@@ -2,23 +2,25 @@ const userAgeQ = prompt("Insert your age");
 
 const userKmQ = prompt("How far are you willing to travel (in km)");
 
+// Defined Km Cost 
+
 const travelKmCost = 0.21 * userKmQ;
+
+// Discount conditions 
 
 if (userAgeQ <= 17) {
   const minorPercentage = (travelKmCost * 20) / 100;
   const overallTravelCostMinor = travelKmCost - minorPercentage;
 
-  alert(
-    overallTravelCostMinor.toFixed(2));
+  alert(overallTravelCostMinor.toFixed(2));
 
 } else if (userAgeQ >= 65) {
   const overPercentage = (travelKmCost * 40) / 100;
   const overallTravelCostOver = travelKmCost - overPercentage;
 
-  alert(
-    overallTravelCostOver.toFixed(2));
-
+  alert(overallTravelCostOver.toFixed(2));
+  
 } else {
-  alert(
-    travelKmCost.toFixed(2));
+
+  alert(travelKmCost.toFixed(2));
 }
