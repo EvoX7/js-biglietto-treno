0a97a685e44c9bc364825135a38196db17121fea
EvoX7 <creativesoultouch@gmail.com>
2022-06-14 14:48:09 +0200

    Starting

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const userAge = prompt ("Insert your age");
+
+const userKm = prompt ("How far are you willing to travel (in km)");
+
+if (userAge <= 17) {
+    console.log("Hai diritto ad uno sconto del 20%");
+} else if
+    (userAge >= 65) {
+    console.log("Hai diritto ad uno sconto del 40%");
+}
+
+
+
+
+
+
+
