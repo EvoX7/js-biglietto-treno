36678fb6fb74f9c28915f43af9b4ffc5ac05deab
EvoX7 <creativesoultouch@gmail.com>
2022-06-14 16:55:50 +0200

    Working on overall cost

--- js/main.js
+++ js/main.js
@@ -1,19 +1,33 @@
-const userAgeQ = parseInt (prompt("Insert your age"));
+const userAgeQ = prompt("Insert your age");
 
-const userKmQ = parseInt (prompt("How far are you willing to travel (in km)"));
+const userKmQ = prompt("How far are you willing to travel (in km)");
+
+const travelKmCost = 0.21 * 100;
+
+const minorPercentage = (userAgeQ * 20) / 100;
+
+const overPercentage = (userAgeQ * 40) / 100;
+
+const overallTravelCostMinor = (travelKmCost + minorPercentage);
+
+const overallTravelCostOver = (travelKmCost + overPercentage);
 
 if (userAgeQ <= 17) {
-  console.log("You will be entitled to a discount of 20% on the ticket.");
+
+    console.log("Your overall ticket cost for your joruney is ${overallTravelCostOver}")
+
 
 } else if (userAgeQ >= 65) {
-    console.log("You will be entitled to a discount of 40% on the ticket.");
-  
+
+    console.log("Your overall ticket cost for your joruney is ${overallTravelCostOver}")
+
 } else {
-  console.log("You are not entitled to a discount.");
+    console.log("You are not entitled to a discount.");
 }
+// to fixed per cifre decimali
 
 
-const KmCost = (0.21 * 1);
-console.log (KmCost);
+
+
 
 
