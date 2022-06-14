be20ea808ee8fc6a28316d0f77bcc2125d53c7f3
EvoX7 <creativesoultouch@gmail.com>
2022-06-14 15:12:50 +0200

    Defining discount conditions

--- js/main.js
+++ js/main.js
@@ -1,17 +1,13 @@
-const userAge = prompt ("Insert your age");
+const userAge = prompt("Insert your age");
 
-const userKm = prompt ("How far are you willing to travel (in km)");
+const userKm = prompt("How far are you willing to travel (in km)");
 
 if (userAge <= 17) {
-    console.log("Hai diritto ad uno sconto del 20%");
-} else if
-    (userAge >= 65) {
-    console.log("Hai diritto ad uno sconto del 40%");
+  console.log("Hai diritto ad uno sconto del 20%");
+
+} else if (userAge > 17 && userAge < 65) {
+  console.log("Non hai diritto ad uno sconto");
+
+} else if (userAge >= 65) {
+  console.log("Hai diritto ad uno sconto del 40%");
 }
-
-
-
-
-
-
-
